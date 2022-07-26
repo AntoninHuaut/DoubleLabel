85f5eb5a350a111def6b25505e25c3d7f1d6de65
Clémence Le Roux <clemence.le-roux@ecole.ensicaen.fr>
2022-07-26 13:38:12 +0200

    update db script

--- back/database/script_db.sql
+++ back/database/script_db.sql
@@ -3,15 +3,20 @@ CREATE TABLE DL_EMOTION(
         emotion_name Varchar (50) NOT NULL
 );
 
-CREATE TABLE DL_ANSWER(
-        id_answer          INTEGER NOT NULL,
+CREATE TABLE DL_EMOTION_RANK(
+        id_answer INTEGER NOT NULL,
         emotion  INTEGER NOT NULL ,
         emotion_rank INTEGER,
+        FOREIGN KEY (emotion) REFERENCES DL_EMOTION(id_emotion),
+        PRIMARY KEY (id_answer,emotion,emotion_rank)
+);
+
+CREATE TABLE DL_ANSWER(
+        id_answer          INTEGER NOT NULL,
         feeling Varchar (800) NOT NULL,
         timestamp_ans Varchar (50) NOT NULL,
         ip_user Varchar (50) NOT NULL,
         id_user INTEGER,
         id_image INTEGER,
-    FOREIGN KEY (emotion) REFERENCES DL_EMOTION(id_emotion),
-    PRIMARY KEY (id_answer,id_user,id_image)
+    PRIMARY KEY (id_answer)
 );
