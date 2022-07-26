2bf98f166c96515f7fa3102e71be745bdef8aecf
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-26 14:50:13 +0200

    Update number btn

--- front/src/components/template/ButtonNumberBadger.tsx
+++ front/src/components/template/ButtonNumberBadger.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@mantine/core';
-import { Dispatch, ReactNode, SetStateAction, useEffect, useMemo, useState } from 'react';
-import { Icon, Number1, Number2, Number3, Number4, Number5, Number6, Number7, Number8, Number9, QuestionMark } from 'tabler-icons-react';
-import { NB_IMAGE } from '../../services/Labels.services';
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react';
+import { Number1, Number2, Number3, Number4, Number5, Number6, Number7, Number8, Number9, QuestionMark } from 'tabler-icons-react';
 
 interface ButtonNumberBadgerProps {
     value: string;
@@ -9,18 +8,19 @@ interface ButtonNumberBadgerProps {
     setLabelPriority: Dispatch<SetStateAction<string[]>>;
 }
 
-const ICONS: Record<number, Icon> = {
-    0: QuestionMark,
-    1: Number1,
-    2: Number2,
-    3: Number3,
-    4: Number4,
-    5: Number5,
-    6: Number6,
-    7: Number7,
-    8: Number8,
-    9: Number9,
+const ICONS: Record<number, JSX.Element> = {
+    0: <QuestionMark />,
+    1: <Number1 />,
+    2: <Number2 />,
+    3: <Number3 />,
+    4: <Number4 />,
+    5: <Number5 />,
+    6: <Number6 />,
+    7: <Number7 />,
+    8: <Number8 />,
+    9: <Number9 />,
 };
+const ICONS_SIZE = Object.keys(ICONS).length;
 
 export function ButtonNumberBadger({ value, labelPriority, setLabelPriority }: ButtonNumberBadgerProps) {
     const [localPriority, setLocalPriority] = useState<number>(0);
@@ -36,17 +36,13 @@ export function ButtonNumberBadger({ value, labelPriority, setLabelPriority }: B
 
     useEffect(() => {
         const index = labelPriority.indexOf(value) + 1;
-        setLocalPriority(index < 0 ? 0 : index % NB_IMAGE);
+        setLocalPriority(index < 0 ? 0 : index % ICONS_SIZE);
     }, [labelPriority, value]);
+
     useEffect(() => setDisabled(localPriority > 0), [localPriority]);
 
-    const NumberIcon = useMemo(() => {
-        const TmpIcon = ICONS[localPriority];
-        return <TmpIcon />;
-    }, [localPriority]);
-
     return (
-        <Button leftIcon={NumberIcon} onClick={onBtnClick} variant="light" color={isDisabled ? 'teal' : 'blue'} size="sm" mr="sm" mb="sm">
+        <Button leftIcon={ICONS[localPriority]} onClick={onBtnClick} variant="light" color={isDisabled ? 'teal' : 'blue'} size="sm" mr="sm" mb="sm">
             {value}
         </Button>
     );
