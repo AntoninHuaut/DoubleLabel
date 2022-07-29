24a470189251a0c1997e1284b8130cd842aaa5d7
Clémence Le Roux <clemence.le-roux@ecole.ensicaen.fr>
2022-07-29 15:44:40 +0200

    check if db exists

--- back/init_db.py
+++ back/init_db.py
@@ -1,16 +1,23 @@
 import sqlite3
+from os.path import exists
 
-with open('./database/double_label_db.db', 'w', encoding='utf-8') as f:
-    create_db = f
+file_exists = exists('./database/double_label_db.db')
 
+print("FILE EXIST : ", file_exists)
+if not file_exists:
+    with open('./database/double_label_db.db', 'w', encoding='utf-8') as f: #creating db file
+        create_db = f
 
-connection = sqlite3.connect('./database/double_label_db.db')
+    connection = sqlite3.connect('./database/double_label_db.db')
 
-for file in ['script_db.sql', 'insert_data.sql', 'insert_images.sql']:
-    with open('./database/' + file) as f:
-        connection.executescript(f.read())
+    for file in ['script_db.sql', 'insert_data.sql', 'insert_images.sql']:
+        with open('./database/' + file) as f:
+            connection.executescript(f.read())
 
-cur = connection.cursor()
+    cur = connection.cursor()
 
-connection.commit()
-connection.close()
+    connection.commit()
+    connection.close()
+    print("Database created - loading")
+else :
+    print("Database already exists - loading")
