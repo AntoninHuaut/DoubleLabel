ab29d8cc041a590be3ddf1a3960600ec31d00582
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-29 15:31:27 +0200

    push

--- back/init_db.py
+++ back/init_db.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-with open('./database/double_label_db.db','w', encoding = 'utf-8') as f:
+with open('./database/double_label_db.db', 'w', encoding='utf-8') as f:
     create_db = f
 
 
 connection = sqlite3.connect('./database/double_label_db.db')
 
-
-with open('./database/script_db.sql') as f:
-    connection.executescript(f.read())
+for file in ['script_db.sql', 'insert_data.sql', 'insert_images.sql']:
+    with open('./database/' + file) as f:
+        connection.executescript(f.read())
 
 cur = connection.cursor()
 
