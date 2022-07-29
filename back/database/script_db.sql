8bc9782fb9aa8e895abba96adcf12670204a4dd3
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-29 15:33:12 +0200

    update structure

--- back/database/script_db.sql
+++ back/database/script_db.sql
@@ -1,28 +1,27 @@
 CREATE TABLE DL_EMOTION(
-        id_emotion   INTEGER  NOT NULL PRIMARY KEY,
+        id_emotion INTEGER NOT NULL PRIMARY KEY,
         emotion_name Varchar (50) NOT NULL
 );
 
 CREATE TABLE DL_EMOTION_RANK(
         id_answer INTEGER NOT NULL,
-        id_emotion  INTEGER NOT NULL ,
+        id_emotion INTEGER NOT NULL,
         emotion_rank INTEGER,
         FOREIGN KEY (id_emotion) REFERENCES DL_EMOTION(id_emotion),
-        PRIMARY KEY (id_answer,id_emotion,emotion_rank)
+        PRIMARY KEY (id_answer, id_emotion, emotion_rank)
 );
 
 CREATE TABLE DL_IMAGE(
-        id_image INTEGER NOT NULL,
-        id_emotion INTEGER NOT NULL ,
-        PRIMARY KEY (id_image,id_emotion)
+        id_image INTEGER NOT NULL PRIMARY KEY,
+        id_emotion INTEGER NOT NULL,
 );
 
 CREATE TABLE DL_ANSWER(
-        id_answer          INTEGER NOT NULL,
+        id_answer INTEGER NOT NULL,
         feeling TEXT NOT NULL,
         timestamp_ans TIMESTAMP,
         ip_user Varchar (50) NOT NULL,
         id_user Varchar(50) NOT NULL,
         id_image INTEGER,
-    PRIMARY KEY (id_answer)
+        PRIMARY KEY (id_answer)
 );
