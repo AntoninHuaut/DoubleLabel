e60e4b5aab44762cf878adcfa5af630ba5701dc2
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-29 15:24:17 +0200

    docker

--- back/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export FLASK_APP=double_label_back
-export FLASK_ENV=production
-export FLASK_DEBUG=off
-export CORS_URL=https://doublelabel.netlify.app
-pip install -r requirements.txt
-python init_db.py
-python -m flask run
